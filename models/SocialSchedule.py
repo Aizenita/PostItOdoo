7acb0faf5bec3c2699fca0abf9ff74ada9b121b6
Sieg04 <35668282+Sieg04@users.noreply.github.com>
2025-02-27 16:25:13 +0100

    Revert "feat:Añadido calendar a Programaciones"

--- models/SocialSchedule.py
+++ models/SocialSchedule.py
@@ -1,38 +1,15 @@
-from odoo import fields, models, api
+from odoo import fields, models
+
 
 class SocialSchedule(models.Model):
     _name = 'postitodoo.socialschedule'
     _description = 'Maneja publicaciones programadas'
 
-    name = fields.Char(
-        string='Nombre Programación',
-        compute='_compute_name',
-        store=True
-    )
-    post_id = fields.Many2one(
-        'postitodoo.socialpost',
-        string='Publicación programada',
-        required=True
-    )
-    scheduled_time = fields.Datetime(
-        string='Fecha y hora de publicación',
-        required=True
-    )
+    post_id = fields.Many2one('postitodoo.socialpost', string='Publicación programada', required=True)
+    scheduled_time = fields.Datetime(string='Fecha y hora de publicación', required=True)
     status = fields.Selection([
         ('scheduled', 'Programada'),
         ('published', 'Publicada'),
         ('pending', 'Pendiente'),
         ('failed', 'Fallida')
     ], string='Estado', default='pending', required=True)
-
-    @api.depends('post_id')
-    def _compute_name(self):
-        """
-        Pone en 'name' el título de la publicación.
-        Si no hay publicación, muestra un texto por defecto.
-        """
-        for record in self:
-            if record.post_id:
-                record.name = record.post_id.name or "Sin título"
-            else:
-                record.name = "Sin publicación"
